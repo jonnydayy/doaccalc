d99b5ab34704983e42eb8dcd5ebea995c30f9b1a
Jonny Day <jonathanday2@Gmail.com>
2024-03-17 20:11:45 +0000

    first bits

--- calc.js
+++ calc.js
@@ -1,4 +1,17 @@
 var weight = ""
 var height = ""
+var gender = ""
+var creat = ""
 var drug = ""
-var 
+var dob = ""
+
+var age = "" //insert logic here to calculate age from dob
+
+
+//work out creatinine clearance
+
+var constant = ""
+//Constant = 1.23 for men; 1.04 for women
+
+var crcl = (((140-age)*weight*)/(creat))
+
