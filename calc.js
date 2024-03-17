790bb02a9062fed0ecba1f2a194ac05cadd973b3
Jonny Day <jonathanday2@Gmail.com>
2024-03-17 20:12:37 +0000

    all

--- calc.js
+++ calc.js
@@ -1,17 +1,66 @@
+console.log("test")
+
+
 var weight = ""
 var height = ""
-var gender = ""
-var creat = ""
+var gender = "1"
+var creat = "11"
 var drug = ""
+var indication = 'AF'
 var dob = ""
-
 var age = "" //insert logic here to calculate age from dob
 
+console.log(weight)
+
+if (age == "") {
+};
 
 //work out creatinine clearance
+if (gender == "M") {
+    var constant = "1.23"
+} else if (gender == "F") {
+    var constant = "1.04"
+};
+var crcl = ((140 - age) * weight * constant) / (creat);
 
-var constant = ""
-//Constant = 1.23 for men; 1.04 for women
+console.log(crcl);
 
-var crcl = (((140-age)*weight*)/(creat))
+//all the diff dosages 
+
+if (drugname = 'Rivaroxaban') {
+    if (indication = 'AF') {
+        //rivaroxaban af dosages
+        if (crcl > 50) {
+            var dose = 'dose as in normal renal function, Rivaroxaban 20mg daily to be taken with food'
+        } else if (crcl >= 30 && crcl <= 50) {
+            var dose = 'Rivaroxaban 15mg once a day'
+        } else if (crcl <= 29 && crcl >= 15) {
+            var dose = 'Use with caution - Rivaroxaban 15mg once a day'
+        } else if (crcl <= 14) {
+            var dose = 'avoid'
+        } else {
+            var dose = 'outside calculation peramiters for rivaroxaban'
+        }
+    } else if (indication = 'DVTN') {
+        //rivaroxaban dvt treatment dosages
+        if (crcl > 50) {
+            var dose = 'dose as in normal renal function, Rivaroxaban 15mg twice a day for 21 days then 20mg once a day to be taken with food'
+        } else if (crcl >= 30 && crcl <= 50) {
+            var dose = 'Rivaroxaban 15mg BD for 3 weeks then 15-20mg once a day'
+        } else if (crcl <= 29 && crcl >= 15) {
+            var dose = 'Use with caution - Rivaroxaban 15mg twice a day for 3 weeks then 15-20mg once daily'
+        } else if (crcl <= 14) {
+            var dose = 'avoid'
+        } else {
+            var dose = 'outside calculation peramiters for rivaroxaban'
+        }
+    } else if (indication = 'DVTO') {
+        //rivaroxaban dvt treatment dosages
+        var dose = 'no specific advice'
+    }
+} else if (drugname = 'Edoxaban') {
+
+}
+
+
 
